c275e7fbf2966ed5651990abef9358eecaf36f1b
YellowPipe <philippdmitrienko@gmail.com>
2019-06-30 15:52:02 +0300

    basic components setup

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Button = () => {
+	return(
+		<h1>World</h1>
+	);
+};
+
+export default Button;
---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from './Button'
+
+const ButtonPanel = () => {
+	return(
+		<Button />
+	);
+};
+
+export default ButtonPanel;
